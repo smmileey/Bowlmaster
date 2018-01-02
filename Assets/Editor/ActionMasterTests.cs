7b1fcb4163ec33559f4d631f4510a5107ef3d25b
smmileey <smmileey@gmail.com>
2018-01-02 18:57:24 +0100

    red->green-> ...

--- Assets/Editor/ActionMasterTests.cs
+++ Assets/Editor/ActionMasterTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Assets.Scripts;
 using NUnit.Framework;
 
@@ -7,30 +8,108 @@ namespace Assets.Editor
 {
     public class ActionMasterTests
     {
-
-        private readonly ActionMaster _systemUnderTest;
-
-        public ActionMasterTests()
-        {
-            _systemUnderTest = new ActionMaster();
-        }
-
         [TestCaseSource(nameof(InvalidPinCountCases))]
-        public void T01_WhenArgumentIsBelow0OrAbove10_OutOfRangeExceptionIsThrown(int pins)
+        public void T01_WhenArgumentIsBelow0OrAbove10_OutOfRangeExceptionIsThrown(int pinsCount)
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => _systemUnderTest.Bowl(pins));
+            Assert.Throws<ArgumentOutOfRangeException>(() => GetSystemUnderTest().Bowl(pinsCount));
         }
 
         [Test]
-        public void T02_WhenStrike_EndTurn()
+        public void T02_WhenStrike_ReturnEndTurn()
         {
-            Assert.AreEqual(AfterStrikeAction.EndTurn, _systemUnderTest.Bowl(10));
+            Assert.AreEqual(AfterStrikeAction.EndTurn, GetSystemUnderTest().Bowl(10));
+        }
+
+        [TestCaseSource(nameof(OneToNinePinsCases))]
+        public void T03_WhenNotAllPinsHitOnSingleThrow_ReturnTidy(int pinsCount)
+        {
+            Assert.AreEqual(AfterStrikeAction.Tidy, GetSystemUnderTest().Bowl(pinsCount));
+        }
+
+        [Test]
+        public void T04_WhenSpare_ReturnEndTurn()
+        {
+            var sut = GetSystemUnderTest();
+            sut.Bowl(2);
+            Assert.AreEqual(AfterStrikeAction.EndTurn, sut.Bowl(8));
+        }
+
+        [Test]
+        public void T05_WhenLastFrameFirstHitUnderTen_ThenReturnTidy()
+        {
+            var sut = GetSystemUnderTest();
+            ProceedToTheLastFrame(sut);
+            Assert.AreEqual(AfterStrikeAction.Tidy, sut.Bowl(2));
+        }
+
+        [Test]
+        public void T06_WhenLastFrameTwoHitsUnderTen_ThenReturnEndGame()
+        {
+            var sut = GetSystemUnderTest();
+            ProceedToTheLastFrame(sut);
+            sut.Bowl(2);
+            Assert.AreEqual(AfterStrikeAction.EndGame, sut.Bowl(3));
+        }
+
+        [Test]
+        public void T07_WhenLastFrameSpare_ThenReturnReset()
+        {
+            var sut = GetSystemUnderTest();
+            ProceedToTheLastFrame(sut);
+            sut.Bowl(2);
+            Assert.AreEqual(AfterStrikeAction.Reset, sut.Bowl(8));
+        }
+
+        [Test]
+        public void T08_WhenLastFrameStrike_ThenReturnReset()
+        {
+            var sut = GetSystemUnderTest();
+            ProceedToTheLastFrame(sut);
+            Assert.AreEqual(AfterStrikeAction.Reset, sut.Bowl(10));
+        }
+
+        [Test]
+        public void T09_WhenLastFrameStrike_AwardThrow_AndReturnEndGame()
+        {
+            var sut = GetSystemUnderTest();
+            ProceedToTheLastFrame(sut);
+            sut.Bowl(10);
+            sut.Bowl(0);
+            Assert.AreEqual(AfterStrikeAction.EndGame, sut.Bowl(5));
+        }
+
+        [Test]
+        public void T10_WheLastFrameSpare_AwardThrow_AndReturnEndGame()
+        {
+            var sut = GetSystemUnderTest();
+            ProceedToTheLastFrame(sut);
+            sut.Bowl(4);
+            sut.Bowl(6);
+            Assert.AreEqual(AfterStrikeAction.EndGame, sut.Bowl(10));
+        }
+
+        private ActionMaster GetSystemUnderTest()
+        {
+            return new ActionMaster();
+        }
+
+        private void ProceedToTheLastFrame(ActionMaster actionMaster)
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                actionMaster.Bowl(10);
+            }
         }
 
         private static IEnumerable<int> InvalidPinCountCases()
         {
             yield return -1;
             yield return 11;
+        }
+
+        private static IEnumerable<int> OneToNinePinsCases()
+        {
+            return Enumerable.Range(1, 9);
         }
     }
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -4,12 +4,51 @@ namespace Assets.Scripts
 {
     public class ActionMaster
     {
+        private readonly int[] _scores = new int[22];
+        private int _throwNumber = 1;
+
         public AfterStrikeAction Bowl(int pins)
         {
             if (pins < 0 || pins > 10) throw new ArgumentOutOfRangeException(nameof(pins));
+            _scores[_throwNumber] = pins;
 
-            if (pins == 10) return AfterStrikeAction.EndTurn;
-            throw new NotImplementedException();
+            if (_throwNumber == 21)
+            {
+                return AfterStrikeAction.EndGame;
+            }
+
+            if (_throwNumber == 20)
+            {
+                _throwNumber++;
+                return _scores[19] + pins >= 10
+                    ? AfterStrikeAction.Reset
+                    : AfterStrikeAction.EndGame;
+            }
+
+            if (pins == 10)
+            {
+                if (_throwNumber == 19)
+                {
+                    _throwNumber++;
+                    return AfterStrikeAction.Reset;
+                }
+
+                _throwNumber += 2;
+                return _throwNumber == 21
+                    ? AfterStrikeAction.Reset 
+                    : AfterStrikeAction.EndTurn;
+            }
+
+            if (_throwNumber % 2 != 0)
+            {
+                _throwNumber++;
+                return AfterStrikeAction.Tidy;
+            }
+            else
+            {
+                _throwNumber++;
+                return AfterStrikeAction.EndTurn;
+            }
         }
     }
 }
