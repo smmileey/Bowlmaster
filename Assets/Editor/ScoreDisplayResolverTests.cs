a6de39fbb32bbb093d462f560dd7af099d9f028f
smmileey <smmileey@gmail.com>
2018-01-23 18:25:02 +0100

    ScoreDisplayResolver unit tests added

--- Assets/Editor/ScoreDisplayResolverTests.cs
+++ Assets/Editor/ScoreDisplayResolverTests.cs
@@ -1,6 +1,177 @@
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Consts;
+using Assets.Scripts.Enums;
+using Assets.Scripts.Managers;
+using Assets.Scripts.Wrappers;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace Assets.Editor
 {
     public class ScoreDisplayResolverTests
     {
+        [Test]
+        public void T01_FirstHitInFrame_FirstFrameUpdate()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers();
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(3, new List<int>(), AfterStrikeAction.Tidy);
+            Assert.AreEqual("3", scoreDisplayWrappers.First().FirstScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.SecondRound, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T02_SecondHitInFrame_SecondFrameUpdate()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers();
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(3, new List<int>(), AfterStrikeAction.Tidy);
+            sut.UpdateScore(5, new List<int> { 8 }, AfterStrikeAction.EndTurn);
+            Assert.AreEqual("5", scoreDisplayWrappers.First().SecondScore.text);
+            Assert.AreEqual("8", scoreDisplayWrappers.First().FrameScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.Completed, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T03_Spare_FirstFrameUpdate_WithSpareSpecialSign()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers();
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(4, new List<int>(), AfterStrikeAction.Tidy);
+            sut.UpdateScore(6, new List<int>(), AfterStrikeAction.EndTurn);
+            Assert.AreEqual("/", scoreDisplayWrappers.First().SecondScore.text);
+            Assert.AreEqual(string.Empty, scoreDisplayWrappers.First().FrameScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.Completed, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T04_Strike_FirstFrameUpdate_WithStrikeSpecialSign()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers();
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(10, new List<int>(), AfterStrikeAction.EndTurn);
+            Assert.AreEqual("X", scoreDisplayWrappers.First().FirstScore.text);
+            Assert.AreEqual(string.Empty, scoreDisplayWrappers.First().SecondScore.text);
+            Assert.AreEqual(string.Empty, scoreDisplayWrappers.First().FrameScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.Completed, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T05_LastRound_FirstHitInFrame_FirstFrameUpdate()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers("F10");
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(3, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Tidy);
+            Assert.AreEqual("3", scoreDisplayWrappers.First().FirstScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.SecondRound, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T06_LastRound_TwoHitsUnderTen_FrameScoreUpdated_AndGameIsCompleted()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers("F10");
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(3, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Tidy);
+            sut.UpdateScore(4, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2, 7 }, AfterStrikeAction.EndGame);
+            Assert.AreEqual("7", scoreDisplayWrappers.First().FrameScore.text);
+            Assert.AreEqual(string.Empty, scoreDisplayWrappers.First().ThirdScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.Completed, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T07_LastRound_StrikeInFirstFrame_FirstFrameUpdatedWithStrikeSpecialSign_AndGameNotCompleted()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers("F10");
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(10, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Reset);
+            Assert.AreEqual("X", scoreDisplayWrappers.First().FirstScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.SecondRound, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T08_LastRound_StrikeInSecondFrame_SecondFrameUpdatedWithStrikeSpecialSign_AndGameNotCompleted()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers("F10");
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(10, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Reset);
+            sut.UpdateScore(10, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Reset);
+            Assert.AreEqual("X", scoreDisplayWrappers.First().SecondScore.text);
+            Assert.AreEqual(string.Empty, scoreDisplayWrappers.First().FrameScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.ThirdRound, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T09_LastRound_AdditionalRoundBySpare_ThirdScoreUpdate_AndGameNotCompleted()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers("F10");
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(3, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Tidy);
+            sut.UpdateScore(7, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Reset);
+            Assert.AreEqual("/", scoreDisplayWrappers.First().SecondScore.text);
+            Assert.AreEqual(string.Empty, scoreDisplayWrappers.First().FrameScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.ThirdRound, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        [Test]
+        public void T10_LastRound_AdditionalRoundByStrike_ThirdScoreUpdate_AndGameNotCompleted()
+        {
+            List<ScoreDisplayWrapper> scoreDisplayWrappers = GetScoreDisplayWrappers("F10");
+            ScoreDisplayResolver sut = new ScoreDisplayResolver(scoreDisplayWrappers);
+
+            sut.UpdateScore(10, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Reset);
+            sut.UpdateScore(3, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, AfterStrikeAction.Reset);
+            sut.UpdateScore(5, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2, 18 }, AfterStrikeAction.EndGame);
+            Assert.AreEqual("5", scoreDisplayWrappers.First().ThirdScore.text);
+            Assert.AreEqual("18", scoreDisplayWrappers.First().FrameScore.text);
+            Assert.AreEqual(ScoreDisplayStatus.Completed, scoreDisplayWrappers.First().ScoreDisplayStatus);
+        }
+
+        /// <summary>
+        /// Text is not mockable, and there is no possibility to create instance of it using ctor. Thus, I'm creating that with my ScoreDisplayResolver class that has its own unit tests implemented.
+        /// </summary>
+        /// <param name="frameIndex"></param>
+        /// <returns></returns>
+        private List<ScoreDisplayWrapper> GetScoreDisplayWrappers(string frameIndex = "F1")
+        {
+            GameObject scoreDisplay = new GameObject();
+
+            GameObject f1 = new GameObject(frameIndex);
+            f1.transform.SetParent(scoreDisplay.transform);
+
+            GameObject one = new GameObject("1") { tag = Tags.FirstScore };
+            one.transform.SetParent(f1.transform);
+            GameObject two = new GameObject("2") { tag = Tags.SecondScore };
+            two.transform.SetParent(f1.transform);
+            GameObject three = new GameObject("3") { tag = Tags.ThirdScore };
+            three.transform.SetParent(f1.transform);
+            GameObject sum = new GameObject("Sum") { tag = Tags.FrameScore };
+            sum.transform.SetParent(f1.transform);
+
+            GameObject text = new GameObject("text");
+            text.AddComponent(typeof(Text));
+            text.transform.SetParent(one.transform);
+            GameObject textTwo = new GameObject("text");
+            textTwo.AddComponent(typeof(Text));
+            textTwo.transform.SetParent(two.transform);
+            GameObject textThree = new GameObject("text");
+            textThree.AddComponent(typeof(Text));
+            textThree.transform.SetParent(three.transform);
+            GameObject textSum = new GameObject("text");
+            textSum.AddComponent(typeof(Text));
+            textSum.transform.SetParent(sum.transform);
+
+            return new ScoreDisplayConverter().Convert(scoreDisplay);
+        }
     }
 }
--- Assets/Scripts/Managers/ScoreDisplayResolver.cs
+++ Assets/Scripts/Managers/ScoreDisplayResolver.cs
@@ -19,6 +19,12 @@ namespace Assets.Scripts.Managers
             _partialScoreDisplays = new Queue<ScoreDisplayWrapper>(partialScoreDisplays);
         }
 
+        /// <summary>
+        /// This method is just for score displaying purpose. It has no validation, so any input that is not valid with basic model bowling game is unpredicted 
+        /// </summary>
+        /// <param name="pinsHitCount">Pins hit in last throw.</param>
+        /// <param name="frameScores">Current frame scores (including current throw).</param>
+        /// <param name="action">Last action performed (after current throw). </param>
         public void UpdateScore(int pinsHitCount, List<int> frameScores, AfterStrikeAction action)
         {
             if (_partialScoreDisplays.Count == 0) return;
@@ -45,7 +51,7 @@ namespace Assets.Scripts.Managers
                                 nextScoreDisplay.SecondScore.text = "X";
                                 nextScoreDisplay.ScoreDisplayStatus = ScoreDisplayStatus.ThirdRound;
                             }
-                            else if (IsSpare(frameScores, nextScoreDisplay.FrameIndex))
+                            else if (IsSpare(pinsHitCount, nextScoreDisplay.FirstScore.text))
                             {
                                 nextScoreDisplay.SecondScore.text = "/";
                                 nextScoreDisplay.ScoreDisplayStatus = ScoreDisplayStatus.ThirdRound;
@@ -60,12 +66,10 @@ namespace Assets.Scripts.Managers
                         }
                         break;
                     case AfterStrikeAction.EndGame:
-                        nextScoreDisplay.ThirdScore.text = IsStrike(pinsHitCount) ? "X" : pinsHitCount.ToString();
+                        if (IsAdditionalRoundGranted(nextScoreDisplay)) nextScoreDisplay.ThirdScore.text = IsStrike(pinsHitCount) ? "X" : pinsHitCount.ToString();
                         nextScoreDisplay.ScoreDisplayStatus = ScoreDisplayStatus.Completed;
                         _frameScoreDisplays.Enqueue(_partialScoreDisplays.Dequeue());
                         break;
-                    default:
-                        throw new NotImplementedException(nameof(action));
                 }
             }
             else
@@ -83,7 +87,7 @@ namespace Assets.Scripts.Managers
                         }
                         else
                         {
-                            nextScoreDisplay.SecondScore.text = IsSpare(frameScores, nextScoreDisplay.FrameIndex) ? "/" : pinsHitCount.ToString();
+                            nextScoreDisplay.SecondScore.text = IsSpare(pinsHitCount, nextScoreDisplay.FirstScore.text) ? "/" : pinsHitCount.ToString();
                         }
                         nextScoreDisplay.ScoreDisplayStatus = ScoreDisplayStatus.Completed;
                         _frameScoreDisplays.Enqueue(_partialScoreDisplays.Dequeue());
@@ -93,11 +97,20 @@ namespace Assets.Scripts.Managers
                         nextScoreDisplay.ScoreDisplayStatus = ScoreDisplayStatus.Completed;
                         _frameScoreDisplays.Enqueue(_partialScoreDisplays.Dequeue());
                         break;
-                    default:
-                        throw new NotImplementedException(nameof(action));
                 }
             }
             ProcessFrameCalculation(frameScores);
+        }
+
+        private bool IsAdditionalRoundGranted(ScoreDisplayWrapper nextScoreDisplay)
+        {
+            var firstScoreText = nextScoreDisplay.FirstScore.text;
+            var secondScoreText = nextScoreDisplay.FirstScore.text;
+            int firstScore, secondScore;
+            return firstScoreText.Equals("X") || secondScoreText.Equals("/") 
+                || int.TryParse(firstScoreText, out firstScore)
+                   && int.TryParse(secondScoreText, out secondScore)
+                   && firstScore + secondScore >= 10;
         }
 
         private bool IsStrike(int pinsHitCount)
@@ -105,9 +118,10 @@ namespace Assets.Scripts.Managers
             return pinsHitCount == 10;
         }
 
-        private bool IsSpare(List<int> frameScores, int frameIndex)
+        private bool IsSpare(int score, string firstFrameScore)
         {
-            return frameScores.Count < frameIndex;
+            int firstFrame;
+            return int.TryParse(firstFrameScore, out firstFrame) && score + firstFrame == 10;
         }
 
         private void Reload()
@@ -117,7 +131,7 @@ namespace Assets.Scripts.Managers
 
         private void ProcessFrameCalculation(List<int> frameScores)
         {
-            if(_frameScoreDisplays.Count == 0) return;
+            if (_frameScoreDisplays.Count == 0) return;
 
             var nextScoreDisplay = _frameScoreDisplays.Peek();
             if (frameScores.Count >= nextScoreDisplay.FrameIndex)
